b88eb00416ef0faf9f29e31ecaec6b9cc5dffb5c
Siavash Ameli <sia.sunrise@gmail.com>
2022-09-25 18:49:10 -0700

    text

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,66 @@ document.addEventListener("DOMContentLoaded", function(){
         cT.removeAttribute("href");
     });
 });
+
+async function typeSentence(sentence, eleRef, delay = 30) {
+    const letters = sentence.split("");
+    let i = 0;
+    while(i < letters.length) {
+        await waitForMs(delay);
+        $(eleRef).append(letters[i]);
+        i++
+    }
+    return;
+}
+
+function waitForMs(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+async function deleteSentence(eleRef, start=0, delay=10) {
+    const sentence = $(eleRef).html();
+    if (sentence) {
+        const letters = sentence.split("");
+        let i = 0;
+        while(letters.length > start) {
+            await waitForMs(delay);
+            letters.pop();
+            $(eleRef).html(letters.join(""));
+        }
+    }
+}
+
+function showCursor(show=true) {
+    var el = document.getElementById("input-cursor");
+    if (show == true) {
+        el.style.display="";
+    } else {
+        el.style.display="none";
+    }
+}
+
+const carouselText = [
+  {text: "Math Geneology"},
+  {text: "ematics"},
+  {text: " Geneology"},
+]
+ var textUpdated = false;
+
+async function typewritter(eleRef) {
+    if (textUpdated == false){
+        textUpdated = true;
+        var el = document.getElementById("sentence");
+        await showCursor(true)
+        await deleteSentence(el, 0, 20);
+        await waitForMs(400);
+        await typeSentence(carouselText[0].text, el, 20);
+        await waitForMs(700);
+        await deleteSentence(el, 4, 15);
+        await waitForMs(400);
+        await typeSentence(carouselText[1].text, el, 30);
+        await waitForMs(500);
+        await typeSentence(carouselText[2].text, el, 15);
+        await waitForMs(700);
+        await showCursor(false)
+    }
+}
