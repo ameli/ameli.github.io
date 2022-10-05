09a06da0bf5f6b61e867707a78123c534641bd77
Siavash Ameli <sia.sunrise@gmail.com>
2022-10-05 16:19:36 -0700

    added files

--- assets/js/adobe_pdf.js
+++ assets/js/adobe_pdf.js
@@ -67,24 +67,30 @@ function showPDF(urlToPDF, slide=false) {
     );
 }
 
-document.addEventListener("adobe_dc_view_sdk.ready", function () {
-
-    document.getElementById("showPDF-thesis-text").addEventListener("click", function () {
-        showPDF("http://transport.me.berkeley.edu/nas/public/files/thesis_text.pdf")
-    });
-
-    document.getElementById("showPDF-thesis-slides").addEventListener("click", function () {
-        showPDF("http://transport.me.berkeley.edu/nas/public/files/thesis_slides.pdf", true)
-    });
-
-    document.getElementById("showPDF-dissertation-text").addEventListener("click", function () {
-        showPDF("http://transport.me.berkeley.edu/nas/public/files/dissertation_text.pdf")
-    });
-
-    document.getElementById("showPDF-dissertation-slides").addEventListener("click", function () {
-        showPDF("http://transport.me.berkeley.edu/nas/public/files/dissertation_slides.pdf", true)
-    });
-});
+// document.addEventListener("adobe_dc_view_sdk.ready", function () {
+//
+//     document.getElementById("showPDF-thesis-text").addEventListener("click", function () {
+//         showPDF("http://transport.me.berkeley.edu/nas/public/files/thesis_text.pdf")
+//     });
+//
+//     document.getElementById("showPDF-thesis-slides").addEventListener("click", function () {
+//         showPDF("http://transport.me.berkeley.edu/nas/public/files/thesis_slides.pdf", true)
+//         // showPDF("https://acrobat.adobe.com/link/track?uri=urn:aaid:scds:US:6cbc754c-b4c6-4e09-97f0-eb3722ae5468", true)
+//         // showPDF("https://berkeley.box.com/s/zjjlltw8qpjsrdxlrwb146cn72xobbss", true)
+//         // showPDF("https://docdro.id/LIcvxAF", true)
+//         // showPDF("https://www.docdroid.net/LIcvxAF/paper-pdf", true)
+//         // showPDF("https://documentcloud.adobe.com/view-sdk-demo/PDFs/Bodea Survey.pdf", true)
+//         // showPDF("https://sia-test.tiiny.site/", true)
+//     });
+//
+//     document.getElementById("showPDF-dissertation-text").addEventListener("click", function () {
+//         showPDF("http://transport.me.berkeley.edu/nas/public/files/dissertation_text.pdf")
+//     });
+//
+//     document.getElementById("showPDF-dissertation-slides").addEventListener("click", function () {
+//         showPDF("http://transport.me.berkeley.edu/nas/public/files/dissertation_slides.pdf", true)
+//     });
+// });
 
 // Add arrayBuffer if necessary i.e. Safari
 (function () {
