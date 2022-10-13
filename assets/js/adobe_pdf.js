86fc51353a5e94f2e39a443bb947660ccddc5e10
Siavash Ameli <sia.sunrise@gmail.com>
2022-10-13 14:43:07 -0700

    pdf on github domain

--- assets/js/adobe_pdf.js
+++ assets/js/adobe_pdf.js
@@ -157,7 +157,8 @@ const pdfData = [
     {
         id: ["view-pdf-cv"],
         // url: "assets/files/cv.pdf",
-        url: "https://github.com/ameli/ameli.github.io/blob/main/assets/files/cv.pdf",
+        url: "https://ameli.github.io/assets/files/cv.pdf",
+        // url: "https://github.com/ameli/ameli.github.io/blob/main/assets/files/cv.pdf",
         // url: "http://transport.me.berkeley.edu/nas/public/files/cv.pdf",
         slide: false,
     },
